fa88b37e9083360acb95c54285747d1289874a82
andre <andre.karyoti.schuro@gmail.com>
2025-02-23 01:17:28 +0100

    added angular ngModel to the form | added prototype validation massages to the form through ngModel logic

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+//imports
+import { Component,  ViewChild } from '@angular/core';
+import { NgModel, FormsModule } from '@angular/forms';
 
+
+//component decorator
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
+
+//component class
 export class ContactComponent {
+
+  @ViewChild('name') nameInput!:NgModel;
+  @ViewChild('email') emailInput!: NgModel;
+  @ViewChild('checkbox') checkboxInput!: NgModel;
 
 }
