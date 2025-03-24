efd412c6bf4c5250220ac398e54ff698e5f69128
andre <andre.karyoti.schuro@gmail.com>
2025-03-24 16:25:01 +0100

    added webflow projects

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -12,9 +12,9 @@ export class AboutMeComponent {
  
   lorem:string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis nulla nisi mollitia molestiae,dolor unde, rerum reprehenderit fugiat, cum beatae eos magni libero expedita nesciunt earum eaque omnis  soluta?';        
      
-  intro:string = 'Ich bin ein leidenschaftlicher Frontend-Entwickler mit einer abgeschlossenen Umschulung in diesem Bereich. Derzeit plane ich, mein Wissen durch eine Weiterbildung im Backend-Development weiter auszubauen.';
-  introExp:string = 'Während meiner Ausbildung konnte ich wertvolle Erfahrungen in der Entwicklung von Webprojekten sammeln - sowohl durch Schulprojekte als auch durch meine Arbeit bei Kombro, einer Marketingfirma, die Webseiten erstellt. Dort war ich für verschiedene Projekte verantwortlich und konnte meine Fähigkeiten in der Praxis weiterentwickeln.';
-  introFuture:string = 'Nun freue ich mich auf die nächste Herausforderung: den Einstieg in eine Entwicklerrolle. Ich bin motiviert, kontinuierlich dazuzulernen, mich weiterzuentwickeln und mein Wissen in spannenden Projekten anzuwenden.';
+  intro:string = 'I am a passionate frontend developer with a completed career change training in this field. Currently, I am planning to expand my knowledge further through additional training in backend development.';
+  introExp:string = 'During my training, I gained valuable experience in web development—both through school projects and my work at Kombro, a marketing company that creates websites. There, I was responsible for various projects and had the opportunity to further develop my skills in practice.';
+  introFuture:string = 'Now, I am excited for the next challenge: stepping into a developer role. I am highly motivated to keep learning, growing, and applying my knowledge to exciting projects.';
 
   windowWidth;
   constructor() {
