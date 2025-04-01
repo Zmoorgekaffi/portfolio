8ec0fd7c678a65c6b47b93a052eaa078efa795d6
andre <andre.karyoti.schuro@gmail.com>
2025-04-01 16:45:20 +0200

    compressed the imgs and convertet them to webp

--- src/app/components/screenshots-wrap/screenshots-wrap.component.ts
+++ src/app/components/screenshots-wrap/screenshots-wrap.component.ts
@@ -40,7 +40,7 @@ export class ScreenshotsWrapComponent {
       github:'https://github.com/Zmoorgekaffi/pokedex',
     },
     {
-      imgSrc: 'assets/imgs/project-imgs/kleine-schwimmer.png',
+      imgSrc: 'assets/imgs/project-imgs/kleine-schwimmer.webp',
       title: 'Kleine Schwimmer',
       tech: 'JavaScript | HTML | CSS | Api | Webflow | Swiper',
       description: 'For the "Kleine Schwimmer", I had the pleasure of developing a beautiful website through the company Kombro. I built it completely from scratch using webflow, ensuring a smooth, functional, and user-friendly experience.',
@@ -49,7 +49,7 @@ export class ScreenshotsWrapComponent {
       webflow: 'https://webflow.com/@andre-karyoti'
     },    
     {
-      imgSrc: 'assets/imgs/project-imgs/enzo.png',
+      imgSrc: 'assets/imgs/project-imgs/enzo.webp',
       title: 'ENZO',
       tech: 'JavaScript | HTML | CSS | Webflow | Swiper',
       description: 'Enzo is a young Swiss musician, and I had the opportunity to develop his website entirely from scratch using webflow. The goal was to create a sleek and engaging online presence that reflects his artistry and passion for music.',
@@ -58,7 +58,7 @@ export class ScreenshotsWrapComponent {
       webflow: 'https://webflow.com/@andre-karyoti'
     },
     {
-      imgSrc: 'assets/imgs/project-imgs/arnet-gartenbau.png',
+      imgSrc: 'assets/imgs/project-imgs/arnet-gartenbau.webp',
       title: 'Arnet Gartenbau',
       tech: 'JavaScript | HTML | CSS | Webflow | Swiper',
       description: 'For Arnet Gartenbau, a landscaping company, I had the chance to develop their website from the ground up using webflow. This project was particularly exciting as the site is structured around a dynamic tagging system, making it both interactive and user-friendly.',
@@ -67,7 +67,7 @@ export class ScreenshotsWrapComponent {
       webflow: 'https://webflow.com/@andre-karyoti'
     },
     {
-      imgSrc: 'assets/imgs/project-imgs/solarhub.png',
+      imgSrc: 'assets/imgs/project-imgs/solarhub.webp',
       title: 'Solarhub',
       tech: 'JavaScript | HTML | CSS | Webflow | Swiper',
       description: 'For Solarhub, a solar panel installation company, I built a lead generation website from scratch using webflow. The focus was on optimizing performance and user flow to efficiently connect potential customers with the right solutions.',
